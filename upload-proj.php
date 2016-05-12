b8e61188d620d07bf73780bb12beb7a401ec97c9
InnaBoycheva <iboychev@ramapo.edu>
2016-05-12 11:30:59 -0400

    Last Comments

--- upload-proj.php
+++ upload-proj.php
@@ -1,21 +1,27 @@
 <?php
 
-   $count = 0;
-   if ( false &&  $_SERVER['REQUEST_METHOD'] == 'POST' ){
+  // This script is responsible for uploading projects for analysis from the
+  // front-end. Everything works as expected and files are successfully added to
+  // the server but I was unable to launch the executable that performs the
+  // actual analysis due to some kind of permission issues.
 
-      $dir = isset( $_POST['proj-name'] ) ? $_POST['proj-name'] : 'NoName';
-      mkdir( 'upload/' . $dir );
+  $count = 0;
+  if ( false &&  $_SERVER['REQUEST_METHOD'] == 'POST' ){
 
-       foreach ( $_FILES['files']['name'] as $i => $name ) {
-           if ( strlen( $_FILES['files']['name'][$i] ) > 1 ) {
-               if ( move_uploaded_file( $_FILES['files']['tmp_name'][$i], 'upload/' . $dir . '/' . $name ) ) {
-                   $count++;
-               }
-           }
-       }
-   }
+    $dir = isset( $_POST['proj-name'] ) ? $_POST['proj-name'] : 'NoName';
+    mkdir( 'upload/' . $dir );
 
-   var_dump( shell_exec( 'C:\\inetpub\\wwwroot\\spinna\\code-explorer.exe' ) );
-   var_dump( shell_exec('powershell C:\\inetpub\\wwwroot\\spinna\\code-explorer.exe') );
+     foreach ( $_FILES['files']['name'] as $i => $name ) {
+         if ( strlen( $_FILES['files']['name'][$i] ) > 1 ) {
+             if ( move_uploaded_file( $_FILES['files']['tmp_name'][$i], 'upload/' . $dir . '/' . $name ) ) {
+                 $count++;
+             }
+         }
+     }
+  }
+
+  // Desperate attempts at launching the exe
+  // var_dump( shell_exec( 'C:\\inetpub\\wwwroot\\spinna\\code-explorer.exe' ) );
+  // var_dump( shell_exec('powershell C:\\inetpub\\wwwroot\\spinna\\code-explorer.exe') );
 
 ?>
